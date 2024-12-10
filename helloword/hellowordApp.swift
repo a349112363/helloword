35425052a86b7290991bd02c7adf79e13e19f714
dt_8705858457 <kiss.loo@qq.com>
2024-12-10 10:02:00 +0800

    1、添加项目

---
+++ helloword/hellowordApp.swift
@@ -0,0 +1,17 @@
+//
+//  hellowordApp.swift
+//  helloword
+//
+//  Created by macmini on 2024/12/10.
+//
+
+import SwiftUI
+
+@main
+struct hellowordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
