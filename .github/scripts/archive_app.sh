26996c09a4179a5798b63a3c8ea82fec11b7c8c7
dt_8705858457 <kiss.loo@qq.com>
2024-12-10 16:44:49 +0800

    提交

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -8,3 +8,15 @@ xcodebuild -workspace helloword.xcworkspace \
             -configuration AdHoc \
             -archivePath $PWD/build/helloword.xcarchive \
             clean archive | xcpretty
+
+
+    - name: Decode and install certificate
+      run: |
+        echo "${{ secrets.CERTIFICATE_PASSWORD }}" | base64 --decode > certificate.p12
+        security import certificate.p12 -P "${{ secrets.CERTIFICATE }}" -A
+        security list-keychains -s build.keychain
+        security set-key-partition-list -S apple-tool:,apple: -s build.keychain
+
+    - name: Decode provisioning profile
+      run: |
+        echo "${{ secrets.PROVISIONING_PROFILE }}" | base64 --decode > ProfilehellowordAdHot.mobileprovision
